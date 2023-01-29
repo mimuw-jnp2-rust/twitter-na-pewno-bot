2510cef398fcfdd99b3708524f4303867937d5a9
mskwr <michal.skwarek@protonmail.ch>
2023-01-29 03:02:09 +0100

    Add tweet generator

--- src/strings.rs
+++ src/strings.rs
@@ -1,4 +1,5 @@
 use std::string::ToString;
+use time::OffsetDateTime;
 use rand::Rng;
 
 const GREETINGS: [&str; 5] = ["Cześć", "Czołem", "Hej", "Serwus", "Witaj"];
@@ -29,4 +30,28 @@ pub fn generate_reply(username: &str) -> String {
         + " "
         + &DIVISION_WORDS[rng.gen_range(0..DIVISION_WORDS.len())].to_string()
         + ".";
+}
+
+// Generates tweet with daily statistics.
+#[allow(dead_code)]
+pub fn generate_tweet(prev_stat: usize, cur_stat: usize) -> String {
+    let diff = prev_stat.abs_diff(cur_stat);
+    let comparison = if prev_stat == cur_stat {
+        "To wynik taki sam jak poprzedniego dnia. 📊".to_string()
+    } else {
+        "To wynik o ".to_string()
+            + &diff.to_string()
+            + if prev_stat < cur_stat { " większy (+" } else { " mniejszy (-" }
+            + &(diff as f32 / prev_stat as f32 * 100.0).round().to_string()
+            + "%) względem poprzedniego dnia. "
+            + if prev_stat < cur_stat { "📈" } else { "📉" }
+    };
+
+    let today = OffsetDateTime::now_utc().date();
+    return "W dniu ".to_owned()
+        + &today.previous_day().expect("invalid date").to_string()
+        + " fraza 'na pewno' została błędnie napisana przez aż "
+        + &cur_stat.to_string()
+        + " użytkowników Twittera.\n\n"
+        + &comparison
 }
